077d14f8e60d7d0e465a6387498f255240a87bee
Thomas Hehn <t.m.hehn@tudelft.nl>
2021-01-09 19:41:02 +0100

    Fix indentation

--- utilities.py
+++ utilities.py
@@ -220,7 +220,7 @@ def do_data_augmentation(data_in, res, nsegs):
                 np_con = np.flip(rows[0:res], 0).to_numpy()
             else:
                 np_con = np.concatenate((np_con, np.flip(rows[i*res:(i+1)*res], 0).to_numpy()))   
-                    np_con = np.concatenate((np_con, np.flip(rows[i*res:(i+1)*res], 0).to_numpy()))   
+                np_con = np.concatenate((np_con, np.flip(rows[i*res:(i+1)*res], 0).to_numpy()))   
                 np_con = np.concatenate((np_con, np.flip(rows[i*res:(i+1)*res], 0).to_numpy()))   
         df_left = df_left.append(pd.DataFrame([np.concatenate((np_con, np.array(['right']), rows[nsegs*res+1:len(rows)]))], columns=columns), ignore_index=True)
         
@@ -376,3 +376,5 @@ def detectionFolder(folder, score_threshold=0, height_threshold=0):
         filtered_detections_per_frame.append(filter_detections)
 
     return filtered_detections_per_frame
+
+
